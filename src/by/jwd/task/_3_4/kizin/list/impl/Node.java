ebd41c2114c6cac0071bb747aaaf2a19da96a7b9
Kosty Kizin <KostyKizin@gmail.com>
2017-12-17 16:04:16 +0300

    s

--- src/by/jwd/task/_3_4/kizin/list/impl/Node.java
+++ src/by/jwd/task/_3_4/kizin/list/impl/Node.java
@@ -1,6 +1,6 @@
 package by.jwd.task._3_4.kizin.list.impl;
 
-public class Node<E> {
+class Node<E> {
     //without access modifier cause we use the class only inside the package.
      Node next;
      Node back;
